3d28c12db661f95e8ab9e9a7dcba5faf2d0f8a88
Nathan Roberts <nathanr@tardislabs.com>
2023-04-14 13:54:39 -0700

    Refactored a bunch of shit in nextrack

--- tools/selector.py
+++ tools/selector.py
@@ -10,6 +10,62 @@ hour = 60 * minute
 day = 24 * hour
 week = 7 * day
 
+def readTrack( track, state, id3, easy ):
+    """Extract relevant data from a track when added to the database.
+
+    When an MP3 is added to the database, or updated, this routine is called to
+    extract any needed information from the ID3 data, etc.
+
+    track: Path to the track (relative to library folder)
+    state: State database (see weighTrack for details)
+    id3: ID3 information, as provided by Mutagen
+    easy: ID3 information, as provided by Mutagen.easyid3
+
+    Return:
+    None expected. Function is expected to insert information into state
+    database as needed.
+    """
+
+    info = state['files'][track]
+
+    # Convert some information into a standard format
+    for field in ('artist', 'title', 'album'):
+        if field in easy:
+            info[field] = easy[field][0]
+        else:
+            info[field] = None
+
+    # Convert NN or NN/NN format into just a plain int
+    if 'tracknumber' in easy:
+        track = easy['tracknumber'][0]
+        track = track.partition( '/' )[0]
+        track = int( track )
+        info['track'] = track
+    else:
+        info['track'] = None
+
+    # Extract boolean flags
+    for field in ('Skip', 'Follow'):
+        fieldl = field.lower()
+        if f"TXXX:{field}" in id3:
+            data = id3[f"TXXX:{field}"][0]
+            if data.lower() in ('1', 't', 'true', 'y', 'yes', 'on'):
+                info[fieldl] = True
+            else:
+                info[fieldl] = False
+        else:
+            info[fieldl] = False
+
+    # Create links for 'follow' tracks.
+    # followid contains the artist, album, and track number. This is
+    # stored in the database whenever the track is played. link contains
+    # (if a track is marked as 'follow') the followid for the track
+    # preceeding it
+    info['followid'] = f"{info['artist']}:{info['album']}:{info['track']}"
+    if info['follow']:
+        info['link'] = f"{info['artist']}:{info['album']}:{info['track'] - 1}"
+
+
 def preSelect( state ):
     """Perform any needed functions before selecting a track"""
 
@@ -39,23 +95,20 @@ def weighTrack( track, state ):
         this way, this script can be called repeatedly for testing purposes,
         and it will simulate an actual set of selections, regardless of 'real'
         time.
-    file: A dict containing the files in the library. The key is the path to
+    files: A dict containing the files in the library. The key is the path to
         the file (relative to the library root). Values include:
 
         lastplay: The time the track was last played, in 'internal' time
         added: The time the track was added, in 'internal' time
         folders: A list of folders the track appears in. If it exists in the
-        folder 'foo/bar', the list will contain ['foo', 'bar']
+            folder 'foo/bar', the list will contain ['foo', 'bar']
         modified: The time the file was last modified, in 'unix' time. Used by
-        the main program to detect when a file has been updated.
-        title: The track title from ID3
-        artist: The track artist from ID3
-        album: The track album from ID3
+            the main program to detect when a file has been updated.
+        len: The duration of the track in seconds
 
-        This will also include raw ID3V2 keys, in addition to the above. For
-        instance, there might be a 'TSSE' tag giving the file encoder, or
-        perhaps any other custom tag you'd care to add. In the event that there
-        are multiple tags of the same time, only the first is saved.
+        Note that this does not include ID3 information about the track by
+        default. Any such information needed by the selection algorithm should
+        be added by the readTrack function.
 
     You can also add items to this dict as part of the selection (or pre- or
     post-selection) process. The only requirement is that the items added are
@@ -65,16 +118,37 @@ def weighTrack( track, state ):
     A tuple containing priority and weight, in that order
     """
 
+    # This is just a dummy that cals doWeighTrack to do the actual work, and
+    # stores some info in the 'debug' field for grepping out of the state file
+
+    res = doWeighTrack( track, state )
+    state['files'][track]['debug'] = f"{track}:{res[0]}.{res[1]}"
+    return res
+
+def doWeighTrack( track, state ):
     priority = 0
     weight = 100
     info = state['files'][track]
 
+    # If a track is marked as 'skip', never play it
+    if info['skip']:
+        return (1000, 100)
+    # If a track is marked as 'follow', never play it, unless the previous track
+    # just played, in which case, play it above all other priorities
+    if info['follow']:
+        if info['link'] == state.setdefault( 'lasttrack', '' ):
+            return (-1000, 100)
+        else:
+            return (1000,100)
+
+    # If a track has been played in the last 3 hours, don't play it
     if( info['lastplay'] != None and 
        info['lastplay'] + (3 * hour) > state['time'] ):
-            # perror( f"Rejecting {track}, too recent" )
             return (100, 100)
-    # perror( f"Setting normal weight for {track}" )
+
+    # If part of the 'deliria' folder, reduce its weight
     if 'deliria' in info['folders']: weight = 50
+
     return (1, weight)
 
 def postSelect( track, state ):
@@ -86,6 +160,11 @@ def postSelect( track, state ):
     for folder in state['files'][track]['folders']:
         state['lastplay'][folder] = state['time']
 
+    info = state['files'][track]
+
+    # Record the selected track's follow id, to facilitate playing successor
+    # track
+    state['lasttrack'] = info['followid']
 
 def perror( *args ):
     print( *args, file=sys.stderr )
